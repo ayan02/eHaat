fd7a6be9f68dbd3d043a8d82e1274f9f629bd29e
Ayan MItra <ayanmitra@Ayans-MacBook-Air.local>
2023-04-16 17:18:31 +0530

    Initial Commit

---
+++ src/main/java/com/unified/ehaat/ElectronicsHaatApplication.java
@@ -0,0 +1,13 @@
+package com.unified.ehaat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsHaatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsHaatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unified/ehaat/ElectronicsHaatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unified.ehaat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsHaatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
